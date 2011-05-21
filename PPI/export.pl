1721f32d754ef12ebfc515d6b55de2edd77be889
Simon Forsberg <simon@madartwork.com>
2011-05-21 23:24:57 +0200

    Added strict and warnings

--- PPI/export.pl
+++ PPI/export.pl
@@ -1,6 +1,8 @@
 #Exports a SQLtable => tab-textfile
 #!/usr/local/bin/perl
 
+use strict;
+use warnings;
 use DBI;
 
 my $table = $ARGV[0];   #table to be exported
@@ -8,23 +10,22 @@ my $outfile = $ARGV[1]; #outfile
 open(UT, ">$outfile");
 
 #Connects to the database. Change to fit your database
-$username = 'root';$password = 'root';$database = 'silver';$hostname = 'localhost';
-$dbh = DBI->connect("dbi:mysql:database=$database;" .
+my $username = 'root';my $password = 'root';my $database = 'PPI';my $hostname = 'localhost';
+my $dbh = DBI->connect("dbi:mysql:database=$database;" .
  "host=$hostname;port=3306", $username, $password);
- 
-$SQL= "SELECT * FROM $table;"; 
-$Select = $dbh->prepare($SQL);
+
+my $SQL= "SELECT * FROM $table;"; 
+my $Select = $dbh->prepare($SQL);
 $Select->execute();
 
-while(@Row=$Select->fetchrow_array){
+while(my @Row=$Select->fetchrow_array){
 	print UT &Skriv(@Row);
 }
 
 sub Skriv{
 	my $TabText;
-	foreach $tabar (@_){
+	foreach my $tabar (@_){
 		$TabText = $TabText . "$tabar\t";
 	}
 	return "$TabText\n";
 }
-print "hej Per\n";
