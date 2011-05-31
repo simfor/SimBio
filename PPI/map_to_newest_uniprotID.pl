959d980796ea35fc5ed6a07bc6d72368c617861c
Simon Forsberg <simon@madartwork.com>
2011-05-31 13:42:48 +0200

    Modified map_to_newest_uniprotID.pl. It now downloads uniprot entries if it doesn't find them locally. Since the entries are named "uniprotID.txt" when stored locally redundant information storage arrises because off the different uniprotID-synonyms. It's not pretty but it works.

--- PPI/map_to_newest_uniprotID.pl
+++ PPI/map_to_newest_uniprotID.pl
@@ -10,7 +10,7 @@ my $outfile = $ARGV[1];    #outfile
 open( my $IN, $infile ) || die "Could not open $infile: $!, $?";
 open( my $UT, ">", "$outfile" ) || die "Could not open $outfile: $!, $?";
 
-my $uniprot_path = "/home/simon/workspace/Data/uniprot_entries/text";
+my $uniprot_path = "/home/simon/workspace/Data/uniprot_entries/bajs";
 
   while (<$IN>) {
 	my @klippt   = split( /\t/, $_ );
@@ -28,14 +28,20 @@ sub map {
 	my $new_uniprotID = $_[0];
 	
 	#Checks if the Uniprot entry exists locally
-	if ( -e "$uniprot_path/$_[0].txt" ) {
-		open( ID_IN, "$uniprot_path/$_[0].txt" );
-		@uniprot_entry = <ID_IN>;
-		close ID_IN;
-	}
-	else {
+	if ( !(-e "$uniprot_path/$_[0].txt") ) {
 		print "$_[0].txt finns inte. Allt är johans fel...\n";
+		my $url = "http://www.uniprot.org/uniprot/$_[0].txt";
+		print "Hämtar uniprot entry för $_[0]\n";
+		my $uniprot_entry = get $url;
+		
+		open(my $entry_UT, ">", "$uniprot_path/$_[0].txt");
+		print $entry_UT $uniprot_entry;
+		close $entry_UT;
 	}
+	
+	open( entry_IN, "$uniprot_path/$_[0].txt" );
+	@uniprot_entry = <entry_IN>;
+	close entry_IN;
 
 	for my $line (@uniprot_entry) {
 		if ( $line =~ m/^AC\s+(.*?);/ ) { #Matches the first UniprotID of the synonyms. 
